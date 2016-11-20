6cba8b240b20a998bbabe6aff1722f10e6bd4cc0
Alan Shortis <alan.shortis@mac.com>
2016-11-20 17:21:22 +0000

    Added favicon, removed posts page and social menu

--- wp-content/themes/ovb/page-posts.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Template Name: Posts
- * Description: All posts as excerpts
- */
-
- $context = Timber::get_context();
- $context['posts'] = Timber::get_posts();
- Timber::render('posts.twig', $context);
