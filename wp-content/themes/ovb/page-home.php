ae81db8a7b7f3a887e060d8d7fc3d5dbb8603681
Alan Shortis <alan.shortis@mac.com>
2016-11-20 07:59:32 +0000

    Instagram feed

--- wp-content/themes/ovb/page-home.php
+++ wp-content/themes/ovb/page-home.php
@@ -1,10 +1,17 @@
 <?php
 /**
- * Template Name: Home
- * Description: Home page
- */
+* Template Name: Home
+* Description: Home page
+*/
 
- $context = Timber::get_context();
- $post = new TimberPost();
- $context['post'] = $post;
- Timber::render('home.twig', $context);
+
+$ig_feed = json_decode(file_get_contents("https://api.instagram.com/v1/users/1473021812/media/recent/?access_token=" . IG_TOKEN . "&count=9"));
+
+
+
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+$context['ig_feed'] = $ig_feed;
+Timber::render('home.twig', $context);
