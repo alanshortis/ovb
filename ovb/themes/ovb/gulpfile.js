3f9bf3926f75d0670c73bda099cabac7392b1853
Alan Shortis <alan.shortis@mac.com>
2017-03-03 16:22:54 +0000

    New gulp tasks, source structure

--- ovb/themes/ovb/gulpfile.js
+++ ovb/themes/ovb/gulpfile.js
@@ -1,102 +1,166 @@
-const pkg = require('./package.json'),
-      browserify = require('browserify'),
-      del = require('del'),
-      gulp = require('gulp'),
-      autoprefixer = require('gulp-autoprefixer'),
-      sourcemaps = require('gulp-sourcemaps'),
-      sass = require('gulp-sass')
-      header = require('gulp-header'),
-      rename = require('gulp-rename'),
-      cssnano = require('gulp-cssnano'),
-      uglify = require('gulp-uglify'),
-      notifier = require('node-notifier'),
-      buffer = require('vinyl-buffer'),
-      source = require('vinyl-source-stream');
+const pkg = require('./package.json');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sassLint = require('gulp-sass-lint');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const browserify = require('browserify');
+const eslint = require('gulp-eslint');
+const uglify = require('gulp-uglify');
+const svgo = require('gulp-svgo');
+const svgstore = require('gulp-svgstore');
+const rename = require('gulp-rename');
+const header = require('gulp-header');
+const notifier = require('node-notifier');
+const buffer = require('vinyl-buffer');
+const source = require('vinyl-source-stream');
+const del = require('del');
+const browserSync = require('browser-sync').create();
 
-const options = {
-  "src": {
-    "sass": "src/sass/**/*.scss",
-    "js": "src/js"
+
+// Paths to sources and built assets.
+const path = {
+  src: {
+    sass: './src/sass',
+    js: './src/js',
+    icons: './src/icons'
   },
-  "dest" : {
-    "css": "",
-    "js": "js"
+  dest: {
+    css: './',
+    js: './js',
+    icons: './icons'
   }
 };
 
-const jsHeader = `/* ${pkg.name} | ${new Date()} */\n`,
-      cssHeader = `/* \nTheme Name: ${pkg.name} \nAuthor: ${pkg.author} \nAuthor URI: http://short.is \nVersion: ${pkg.version} \nBuild: ${new Date()}\n*/\n`
+
+// Header comment for theme CSS and for minified assets.
+const cssHeader = `/*\n
+                  Theme Name: ${pkg.name}\n
+                  Description: ${pkg.description}\n
+                  Author: ${pkg.author}\n
+                  Version: ${pkg.version}\n
+                  */\n\n`;
+
+const minifiedHeader = `/* ${pkg.name} ${pkg.version} | ${new Date()} */\n`;
 
 
-// Build CSS from SASS with sourcemaps and autoprefix.
-gulp.task('css', () => {
-  return gulp.src(options.src.sass)
-    .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'expanded'})
+// CSS: Build from SASS, add prefixes and sourcemaps.
+gulp.task('css', ['sasslint'], () => {
+  return gulp.src(`${path.src.sass}/**/*.scss`)
+    .pipe(sass({outputStyle: 'expanded'}))
     .on('error', function(err) {
       sass.logError.call(this, err);
       notifier.notify({
         title: 'Gulp',
-        message: 'SASS error - see terminal for details.'
+        message: 'SASS error'
       });
-    }))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
+    })
+    .pipe(autoprefixer({browsers: ['last 2 versions']}))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
     .pipe(header(cssHeader))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(options.dest.css));
+    .pipe(gulp.dest(path.dest.css))
+    .pipe(browserSync.stream());
+});
+
+
+// Lint our SASS files. See '.sass-lint.yml' for rules.
+gulp.task('sasslint', () => {
+  return gulp.src(`${path.src.sass}/**/*.scss`)
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+    .on('error', () => {
+      notifier.notify({
+        title: 'Gulp',
+        message: 'SASS liniting failed'
+      });
+    });
 });
 
 
 // Minify CSS.
 gulp.task('minify', ['css'], () => {
-  return gulp.src(`style.css`)
+  return gulp.src(`${path.dest.css}/style.css`)
     .pipe(rename({
       suffix: '.min'
     }))
     .pipe(cssnano())
-    .pipe(header(cssHeader))
-    .pipe(gulp.dest(options.dest.css));
+    .pipe(header(minifiedHeader))
+    .pipe(gulp.dest(path.dest.css));
 });
 
 
-// Build our JS modules.
-gulp.task('js', () => {
-  return browserify({entries: `${options.src.js}/app.js`, extensions: ['.js'], debug: true})
+// JavaScript: Build from partials, add sourcemaps.
+gulp.task('js', ['eslint'], () => {
+  return browserify({entries: `${path.src.js}/ovb.js`, extensions: ['.js'], debug: true})
     .bundle()
-    .pipe(source('app.js'))
+    .pipe(source('ovb.js'))
     .pipe(buffer())
-    .pipe(sourcemaps.init())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(`${options.dest.js}/`));
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(path.dest.js))
+    .pipe(browserSync.stream());
 });
 
 
-// Remove debug stuff (console.log, alert), uglify and save with a .min suffix.
+// Lint our JavaScript. See '.eslintrc' for rules.
+gulp.task('eslint', () => {
+  return gulp.src(['gulpfile.js', `${path.src.js}/*.js`])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .on('error', () => {
+      notifier.notify({
+        title: 'Gulp',
+        message: 'JS liniting failed'
+      });
+    });
+});
+
+
+// Uglify JavaScript.
 gulp.task('uglify', ['js'], () => {
-  return gulp.src([`${options.dest.js}/*.js`, `!${options.dest.js}/**/*.min.js`])
+  return gulp.src([`${path.dest.js}/*.js`, `!${path.dest.js}/*.min.js`])
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(uglify({'mangle': false}))
-    .pipe(header(jsHeader))
-    .pipe(gulp.dest(options.dest.js));
+    .pipe(uglify())
+    .pipe(header(minifiedHeader))
+    .pipe(gulp.dest(path.dest.js));
 });
 
 
-// Remove generated assets.
+// Make an SVG sprite from individual icons.
+gulp.task('icons', () => {
+  return gulp.src(`${path.src.icons}/*.svg`)
+    .pipe(svgo({
+      plugins: [{removeStyleElement: true}]
+    }))
+    .pipe(rename({
+      prefix: 'icon-'
+    }))
+    .pipe(svgstore())
+    .pipe(gulp.dest(path.dest.icons));
+});
+
+
+// Delete generated content.
 gulp.task('clean', () => {
-  return del.sync(['./style.*', 'js']);
+  return del.sync([path.dest.js, `${path.dest.css}/*.css`, path.dest.icons]);
 });
 
 
-// Watch for changes.
+// Watch for changes and update with browserSync.
 gulp.task('watch', () => {
-  gulp.watch(options.src.sass, ['css']);
-  gulp.watch(options.src.js, ['js']);
+  browserSync.init({
+    proxy: 'http://ovb.local',
+    files: ['./**/*.php', './**/*.twig'] // Inject HTML when PHP or twig files change.
+  });
+  gulp.watch(`${path.src.sass}/**/*.scss`, ['css']); //
+  gulp.watch(`${path.src.js}/**/*.js`, ['js']);
 });
 
 
-// Default task.
-gulp.task('default', ['clean', 'minify', 'uglify']);
+// Do it all (except watch).
+gulp.task('default', ['clean', 'minify', 'uglify', 'icons']);
--- ovb/themes/ovb/index.php
+++ ovb/themes/ovb/index.php
@@ -1,8 +1,6 @@
 <?php
 
-
 $ig_feed = json_decode(file_get_contents("https://api.instagram.com/v1/users/1473021812/media/recent/?access_token=" . getenv('IG_API') . "&count=9"));
-
 
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
