347361a710ec9cfd86cbecc0491fd79b85fd3ef4
Alan Shortis <alan.shortis@mac.com>
2017-08-11 19:23:46 +0100

    Hide spinner once loaded

--- src/js/modules/_instagram.js
+++ src/js/modules/_instagram.js
@@ -1,32 +1,31 @@
-function igFeed() {
+function _gramHtml(link, thumb) {
+  return `
+    <li>
+      <a href="${link}">
+        <img src="${thumb}" alt="@ovbcycling on Instagram">
+      </a>
+    </li>
+  `;
+}
 
+function igFeed() {
   const user = 1473021812;
   const token = '1473021812.4c0225d.aaa48611504b4784b7e9cae49f95554c';
   const count = 9;
   const grams = [];
   const gramContainer = document.getElementById('js-instagram');
 
-  function gramHtml(link, thumb, alt) {
-    return `
-      <li>
-        <a href="${link}">
-          <img src="${thumb}" alt="${alt}">
-        </a>
-      </li>
-    `;
-  }
-
   fetch(`https://api.instagram.com/v1/users/${user}/media/recent/?access_token=${token}&count=${count}`)
     .then(response => response.json())
     .then(json => grams.push(...json.data))
     .then(() => {
       grams.forEach(gram => {
-        console.log(gram);
         gramContainer.insertAdjacentHTML(
           'beforeend',
-          gramHtml(gram.link, gram.images.thumbnail.url, gram.caption)
+          _gramHtml(gram.link, gram.images.thumbnail.url)
         );
       });
+      document.getElementById('js-spinner').style.display = 'none';
     });
 }
 
