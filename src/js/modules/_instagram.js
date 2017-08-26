6087022dac8af3581504d8d9bcbcae067b39a61c
Alan Shortis <alan.shortis@mac.com>
2017-08-26 23:00:11 +0100

    Rewritten instagram feed

--- src/js/modules/_instagram.js
+++ src/js/modules/_instagram.js
@@ -17,22 +17,30 @@ function igFeed() {
   const user = 1473021812;
   const token = '1473021812.4c0225d.aaa48611504b4784b7e9cae49f95554c';
   const count = 6;
-  const grams = [];
   const gramContainer = document.getElementById('js-instagram');
+  let maxId, scriptElement;
 
-  fetch(`https://api.instagram.com/v1/users/${user}/media/recent/?access_token=${token}&count=${count}`)
-    .then(response => response.json())
-    .then(json => grams.push(...json.data))
-    .then(() => {
-      grams.forEach(gram => {
-        gramContainer.insertAdjacentHTML(
-          'beforeend',
-          _gramHtml(gram.link, gram.images.low_resolution.url, gram.images.standard_resolution.url)
-        );
-      });
-      document.getElementById('js-spinner').style.display = 'none';
-      document.getElementById('js-load-more').style.display = 'block';
+  window._renderGrams = (response) => {
+    maxId = response.data[5].id;
+    const grams = response.data;
+    grams.forEach(gram => {
+      gramContainer.insertAdjacentHTML(
+        'beforeend',
+        _gramHtml(gram.link, gram.images.low_resolution.url, gram.images.standard_resolution.url)
+      );
     });
+    document.getElementById('js-spinner').style.display = 'none';
+    document.getElementById('js-load-more').style.display = 'block';
+  }
+
+  function _getGrams() {
+    scriptElement = document.createElement('script');
+    scriptElement.setAttribute('src', `https://api.instagram.com/v1/users/${user}/media/recent/?access_token=${token}&count=${count}&max_id=${maxId}&callback=_renderGrams` );
+    document.body.appendChild(scriptElement);
+  }
+  _getGrams();
+
+  document.getElementById('js-load-more').addEventListener('click', _getGrams);
 }
 
 module.exports = {
