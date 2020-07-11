89be6c26e7316e9b1c57070d7f8f1e343592b8ff
Alan Shortis <alan.shortis@mac.com>
2020-07-11 20:02:38 +0100

    Minified CSS in production

---
+++ src/_data/site.js
@@ -0,0 +1,7 @@
+module.exports = {
+  isProd: process.env.ELEVENTY_ENV === 'production',
+  title: 'ØVB',
+  description: 'ØVB is a cycling club',
+  url: 'https://ovbcycling.com',
+  twitter: '@ovbcycling',
+};
