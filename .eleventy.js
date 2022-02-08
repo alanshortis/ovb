7efb5bac4593bf29fe0bac5eea9e8d2802febc94
Alan Shortis <alan.shortis@mac.com>
2022-02-08 21:20:56 +0000

    Add avif images

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addLayoutAlias('main', 'layouts/main.njk');
-  eleventyConfig.setTemplateFormats(['njk', 'css', 'svg', 'jpg', 'png', 'webp', 'woff2']);
+  eleventyConfig.setTemplateFormats(['njk', 'css', 'svg', 'jpg', 'png', 'webp', 'avif', 'woff2']);
 
   return {
     dir: {
